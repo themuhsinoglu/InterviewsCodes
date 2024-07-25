bda6457134ec4b4c544db8930b68525e595517f4
themuhsinoglu <muhsinogluahmet@gmail.com>
2024-07-26 01:10:10 +0300

    extension

---
+++ CodesOfInterview/Extension/Program.cs
@@ -0,0 +1,6 @@
+﻿using Extension;
+
+string text = "Hello, how are you today?";
+        int wordCount = text.WordCount(); // Extension method kullanımı
+        Console.WriteLine($"Word count: {wordCount}");
+
---
+++ CodesOfInterview/Extension/StringExtensionMethod.cs
@@ -0,0 +1,14 @@
+﻿namespace Extension;
+
+public static class StringExtensionMethod
+{
+    public static int WordCount(this string str){
+
+        if(string.IsNullOrEmpty(str)){
+            return 0;
+        }
+
+        return str.Split(new char[] { ' ','.','?'}, StringSplitOptions.RemoveEmptyEntries).Length;
+
+    }
+}
