5ff0c6491642a315f02dcabbe3518308ab0c8d6e
themuhsinoglu <muhsinogluahmet@gmail.com>
2024-07-26 00:45:18 +0300

    first

---
+++ CodesOfInterview/Reflection/Person.cs
@@ -0,0 +1,13 @@
+﻿namespace Reflection;
+
+public class Person
+{
+    public string Name { get; set; }
+    public int Age { get; set; }
+
+    public void SayHello()
+    {
+        Console.WriteLine($"Hello, my name is {Name}.");
+    }
+
+}
---
+++ CodesOfInterview/Reflection/Program.cs
@@ -0,0 +1,28 @@
+﻿using System.Reflection;
+using Reflection;
+
+Type type = typeof(Person);
+
+        // Tipin tam adını yazdırma
+        Console.WriteLine("Class Name: " + type.Name);
+
+        // Özellikleri listeleme
+        PropertyInfo[] properties = type.GetProperties();
+        foreach (var property in properties)
+        {
+            Console.WriteLine("Property: " + property.Name + ", Type: " + property.PropertyType.Name);
+        }
+
+        // Metotları listeleme
+        MethodInfo[] methods = type.GetMethods();
+        foreach (var method in methods)
+        {
+            Console.WriteLine("Method: " + method.Name);
+        }
+
+        // Dinamik olarak nesne oluşturma ve metod çağırma
+        object? personInstance = Activator.CreateInstance(type);
+        MethodInfo? sayHelloMethod = type.GetMethod("SayHello");
+        type.GetProperty("Name")!.SetValue(personInstance, "John Doe");
+        sayHelloMethod!.Invoke(personInstance, null);
+
